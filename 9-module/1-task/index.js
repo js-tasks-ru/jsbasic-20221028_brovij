ecc0b47fcdf2cfe6da8105f3508d8e57c7c32d02
Sheldy <pagedevelopernet@gmail.com>
2022-11-30 14:26:48 +0400

    9-module, 1-task

--- 9-module/1-task/index.js
+++ 9-module/1-task/index.js
@@ -1,3 +1,11 @@
 export default function promiseClick(button) {
-  // ваш код...
+  let promise = new Promise((resolve) => {
+
+    button.addEventListener('click', (event) => {
+      resolve(event)
+    })
+
+  })
+
+  return promise
 }
